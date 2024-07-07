fe12064961a41f5e06c8d521ca6029cbb4976dcf
Sparky678 <aryo.widandi@gmail.com>
2024-07-07 10:50:55 +0700

    Memisahkan css dan js ke file yang terpisah Note : - kode css disimpan di folder style di file styles.css - kode javascript disimpan di folder  scripts di file index.js - Menambahkan src="bundle.js" di bagian tag script index.html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,27 @@
+import "/src/style/styles.css"
 
+document.getElementById('signUpBtn').addEventListener('click', function() {
+    window.location.href = '#';
+});
+document.getElementById('logInBtn').addEventListener('click', function() {
+    window.location.href = '#';
+});
+document.getElementById('getProductBtn').addEventListener('click', function() {
+    window.location.href = '#';
+});
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    window.location.href = '#';
+});
+document.getElementById('getDemoBtn').addEventListener('click', function() {
+    window.location.href = '#';
+});
+
+document.querySelectorAll('ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
